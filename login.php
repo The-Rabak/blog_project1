9f38181aac58aa8422b80fe80a422ceb572ac4b5
The-Rabak <44833742+The-Rabak@users.noreply.github.com>
2018-12-22 14:44:48 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -4,30 +4,49 @@ require_once "func.php";
 $link = db_connect();
 $err = '';
 $is_valid_form = true;
-if(isset($_POST['register'])){
-    if(empty($_POST['log_name'])){
-        $err = "We can't just call you Dude, now can we?";
-        $is_valid_form = false;
-    }
-    elseif(empty($_POST['log_pwd'])){
-        $err = "Well just leave the front door open why don't ya";
-        $is_valid_form = false;
-    }
-    elseif(empty($_POST['log_email'])){
-        $err = "There's no way you'll miss out on our newsletter";
-        $is_valid_form = false;
-    }
-    else{
-        $reg_name = trim(filter_input(INPUT_POST,$_POST['reg_name'],FILTER_SANITIZE_STRING));
-        $reg_email = strtolower(filter_input(INPUT_POST,$_POST['reg_email'],FILTER_SANITIZE_EMAIL));
-        $reg_pwd = trim(filter_input(INPUT_POST,$_POST['reg_pwd'],FILTER_SANITIZE_STRING));
-        $reg_query = "INSERT INTO users (uname, email, pwd) VALUES ('$reg_name', '$reg_email', '$reg_pwd')";
+if (isset($_POST['login'])) {
+    if ($_POST['token'] == $_SESSION['csrf_token']) {
 
-        if($reg_request = mysqli_query($link,$reg_query)){
-            header("location: thanks.php");
+        if (empty($_POST['log_pwd'])) {
+            $err = "Well just leave the front door open why don't ya";
+            $is_valid_form = false;
+        } elseif (empty($_POST['log_email'])) {
+            $err = "There's no way you'll miss out on our newsletter";
+            $is_valid_form = false;
+        } else {
+
+            $log_email = strtolower(filter_input(INPUT_POST, 'log_email', FILTER_SANITIZE_EMAIL));
+            $log_pwd = trim(filter_input(INPUT_POST, 'log_pwd', FILTER_SANITIZE_STRING));
+            $log_query = "SELECT * FROM users WHERE email = '$log_email';";
+            if ($log_request = mysqli_query($link, $log_query)) {
+                if (mysqli_num_rows($log_request) > 0) {
+
+                    $result = mysqli_fetch_assoc($log_request);
+
+                    if (password_verify($log_pwd, $result['pwd'])) {
+
+                        $_SESSION['id'] = $result['id'];
+                        $_SESSION['uname'] = $result['uname'];
+                        $_SESSION['email'] = $result['email'];
+                        $_SESSION['user_agent'] = $_SERVER['HTTP_USER_AGENT'];
+                        $_SESSION['ip_addr'] = $_SERVER['REMOTE_ADDR'];
+
+                         header("location: blog.php");
+                    } else {
+                        $err = 'password incorrect';
+                    }
+                } else {
+                    $err = 'email not found';
+                }
+            }
         }
     }
+    else{
+        $err = "please play nice";
+    }
 }
+$csrf_token = hash("sha256", rand(300,9999999));
+$_SESSION['csrf_token'] = $csrf_token;
 
 ?>
 <html lang="en">
@@ -39,19 +58,20 @@ if(isset($_POST['register'])){
 
     <link href='https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic' rel='stylesheet'>
     <script
-        src="https://code.jquery.com/jquery-3.3.1.min.js"
-        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-        crossorigin="anonymous"></script>
+            src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="login-page">
     <div class="form">
 
         <form class="" method="post" action="#">
-            <input type="text" name="log_uname" placeholder="username"/>
+            <input type="email" name="log_email" placeholder="email"/>
             <input type="password" name="log_pwd" placeholder="password"/>
             <input type="submit" class="button1" name="login" value="login"/>
             <span style="color: firebrick;"><?= $err; ?>  </span>
+            <input type="hidden" name="token" value="<?= $csrf_token ?>">
         </form>
     </div>
 </div>
